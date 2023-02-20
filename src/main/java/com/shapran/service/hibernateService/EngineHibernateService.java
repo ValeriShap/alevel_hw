7ee1af7cb7a66e22ee23cac9d686978407b7fa8a
ValeriShap <lerogrey@gmail.com>
2023-02-20 15:24:13 +0200

    Hw24

--- src/main/java/com/shapran/service/hibernateService/EngineHibernateService.java
+++ src/main/java/com/shapran/service/hibernateService/EngineHibernateService.java
@@ -1,17 +1,21 @@
 package com.shapran.service.hibernateService;
 
 import com.shapran.model.Engine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
 public class EngineHibernateService {
     private static final Random random = new Random();
     private static final String[] engineType = {"Gas", "Diesel", "Electric"};
+    private final static Logger LOGGER = LoggerFactory.getLogger(EngineHibernateService.class);
 
     public Engine createEngine(){
         Engine engine = new Engine();
         engine.setPower(random.nextInt(1000));
         engine.setType(engineType[random.nextInt(engineType.length)]);
+        LOGGER.info("Engine was created with power{} and type{}", engine.getPower(), engine.getType());
         return engine;
     }
 }
