47f16ce1c31fdac6c5189b2d2332124412248141
ValeriShap <lerogrey@gmail.com>
2023-02-21 15:26:22 +0200

    Hw25

---
+++ src/main/java/com/shapran/model/CarFactory.java
@@ -0,0 +1,20 @@
+package com.shapran.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CarFactory {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CarFactory.class);
+
+    public Car.CarBuilder createCar(Type type){
+        Car.CarBuilder carBuilder = null;
+        if (type == Type.CAR){
+            carBuilder = new PassengerCar.PassengerCarBuilder();
+        }else if (type == Type.TRUCK){
+            carBuilder = new Truck.TruckBuilder();
+        }else{
+            LOGGER.warn("Another {} type of car", type);
+        }
+        return carBuilder;
+    }
+}
